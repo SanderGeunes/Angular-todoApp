0942bf88aedb1d5b440e0725e549ddf6651ddb06
SanderGeunes <sandergeunes@gmail.com>
2023-01-04 14:30:28 +0100

    Added routing (manually)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule,Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { OpenComponent } from './open/open.component';
 import { DoneComponent } from './done/done.component';
+
+// routing was added in the app.module because it was done manually, since Angular didn't provide 
+// the routing option upon creating the new project 
+// egghead.io was used as a source for this action
+const routes: Routes = [
+  {path: 'open', component: OpenComponent},
+  {path: '', redirectTo: '/open', pathMatch: 'full'},
+  {path: 'done', component: DoneComponent}
+];
 
 @NgModule({
   declarations: [
@@ -12,7 +22,8 @@ import { DoneComponent } from './done/done.component';
     DoneComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
