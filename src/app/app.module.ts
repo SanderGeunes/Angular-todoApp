285bc4115a782684c8a2fe76fc5d7fb1096b439b
SanderGeunes <sandergeunes@gmail.com>
2023-01-04 13:30:15 +0100

    Added 2 components (open/done)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OpenComponent } from './open/open.component';
+import { DoneComponent } from './done/done.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OpenComponent,
+    DoneComponent
   ],
   imports: [
     BrowserModule
