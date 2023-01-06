a7d5310016b2e75e5ea4221b5d64a3346bb5ba03
SanderGeunes <sandergeunes@gmail.com>
2023-01-06 14:46:46 +0100

    created home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,24 @@ import { RouterModule,Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { OpenComponent } from './open/open.component';
 import { DoneComponent } from './done/done.component';
+import { HomeComponent } from './home/home.component';
 
 // routing was added in the app.module because it was done manually, since Angular didn't provide 
 // the routing option upon creating the new project 
 // egghead.io was used as a source for this action
 const routes: Routes = [
   {path: 'open', component: OpenComponent},
-  {path: '', redirectTo: '/open', pathMatch: 'full'},
-  {path: 'done', component: DoneComponent}
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'done', component: DoneComponent},
+  {path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     OpenComponent,
-    DoneComponent
+    DoneComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
